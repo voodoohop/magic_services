9057710b3bcfe47d6a61291415ecefa34774e96d
Thomas <thomas@pixtunes.com>
2020-02-18 16:25:16 +0100

    Separated CLI from index

--- index.js
+++ index.js
@@ -1,66 +1,7 @@
 
 const discovery = require("./discovery");
-const {autoexpose, AUTOEXPOSER_SERVICE_TYPE} = require("./discovery_autoexpose");
-const nodeCleanup = require('node-cleanup');
-const openBrowser = require('opn');
+const { autoexpose } = require("./discovery_autoexpose");;
 
-const { publishService, findServiceOnce } = discovery;
-
-if (require.main === module) {
-    console.log('called directly');
-    const program = require('commander');
-
-    program
-        .option('--expose <name@host:port>', 'Expose local service')
-        .option('--expose-metadata <metadata>', 'Metadata in the form.')
-        .option('--no-autoexpose', 'Don\'t start an EC2-based exposer proxy.', false)
-        .option('--launch-visualizer', 'Open the network\'s service visualizer in the browser.', false)
-        .outputHelp()
-    program.parse(process.argv);
-
-
-
-    console.log("Arguments", program.opts());
-
-    if (program.autoexpose)
-        autoexpose();
-
-    if (program.expose) {
-        console.log("Exposing...");
-        exposeService(program);
-    }
-        
-    if (program.launchVisualizer) {
-        launchVisualizer()
-    }
-
-    
-} else {
-    autoexpose();
-    console.log('required as a module');
-}
-
+autoexpose();
 
 module.exports = discovery;
-
-async function launchVisualizer() {
-   console.log("Looking for visualizer service...");
-   const service = await findServiceOnce({type:AUTOEXPOSER_SERVICE_TYPE}, 20000);
-   console.log("Got visualizer service", service);
-   openBrowser(`http://${service.host}:${service.port}`);
-}
-
-async function exposeService(program) {
-    const [name, host_and_port] = program.expose.split("@");
-    const [host, port] = host_and_port.split(":");
-    let metadata = {};
-    if (program.exposeMetadata) {
-        const keyvalues = program.exposeMetadata.split(",");
-        keyvalues.forEach(keyvalue => {
-            const [key, value] = keyvalue.split(":");
-            metadata[key] = value;
-        });
-    }
-    const unpublish = await publishService({ type: name, port, host, txt: metadata });
-    nodeCleanup(unpublish);
-}
